dc0b19a438abf31f39dab869680105fafc2a6d26
jwnelen <jeroentjuuhh@gmail.com>
2020-01-23 22:41:45 +0100

    Start with adding users to events but not completely working

--- controllers/eventsController.js
+++ controllers/eventsController.js
@@ -1,5 +1,4 @@
 var Event = require('../models/event');
-
 var User = require('../models/user.js');
 var async = require('async');
 
@@ -14,21 +13,41 @@ exports.index = function (req, res, next) {
 };
 
 exports.events_detail = function (req, res, next) {
-    Event.findById(req.params.id)
-        .exec(function (err, event) {
-            if (err) {
-                return next(err)
-            }
-
-            res.render('event_detail', {eventItem: event});
-        });
+    async.parallel({
+        event_: function (callback) {
+            Event.findById(req.params.id, callback)
+        },
+    }, function (err, results) {
+        if (err) {
+            next(err)
+        }
+        console.log(results.event_);
+        res.render('event_detail', {eventItem: results.event_, amount_sign_ups: results.event_.sign_ups.length});
+    });
 };
 
 exports.event_register_post = function (req, res, next) {
     // User is not signed in yet
+    // sign_ups
     if (res.locals.currentUser) {
-        console.log('locals current user: ', res.locals.currentUser);
-        res.send('Clicked the button');
+        User.findById(res.locals.currentUser)
+            .exec(function (err, user) {
+                    if (err) {
+                        return next(err);
+                    }
+                    Event.findByIdAndUpdate(
+                        req.params.id,
+                        {$push: {"sign_ups": user}},
+                        {safe: true, upsert: true, new: true},
+                        function (error, succes) {
+                            if (error) {
+                                console.log(error);
+                            } else {
+                                res.redirect('/events/' + req.params.id);
+                            }
+                        });
+                }
+            );
     } else {
         res.redirect('/users/login');
     }
