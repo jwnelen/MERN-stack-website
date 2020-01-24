f64549294186c6016faf076d717b03dc0da6c599
jwnelen <jeroentjuuhh@gmail.com>
2020-01-24 17:10:09 +0100

    More fields added to UserSchema

--- models/user.js
+++ models/user.js
@@ -3,7 +3,11 @@ var passportLocalMongoose = require("passport-local-mongoose");
 
 var UserSchema = new mongoose.Schema({
     username: String,
-    password: String
+    password: String,
+    full_name: String,
+    gender: String,
+    email: String,
+    date_of_birth: Date
 });
 
 UserSchema.plugin(passportLocalMongoose);
